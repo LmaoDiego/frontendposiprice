c7233422a5e2e9372f9cc4100a0c072864977979
PC <Lmaodiego@gmail.com>
2021-06-14 09:06:59 -0500

    add: Product Page

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  }
+  ,
+  {
+    path: '/products',
+    name: 'products',
+    component: () => import('../components/products')
+  },
+  {
+    path: '/products/new',
+    name: 'add-product',
+    component: () => import('../components/add-product')
+  },
+  {
+    path: '/product/:id',
+    name: 'edit-product',
+    component: () => import('../components/edit-product')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
